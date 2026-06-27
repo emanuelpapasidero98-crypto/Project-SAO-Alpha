23c6427d789fbc9867bde44aa7272284d1d02c27
Z User <z@container>
2026-06-27 13:43:44 +0000

    Fix: useRef non importato in SaoHUD (causava crash al mount)

--- src/components/sao/SaoHUD.tsx
+++ src/components/sao/SaoHUD.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useSaoSound } from '@/hooks/useSaoSound';
 
 /**
